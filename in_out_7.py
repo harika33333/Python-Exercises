c1a210ac503466c3c779c2481e4fd8dffb072a4f
harika33333 <harika9653@gmail.com>
2025-06-21 22:26:03 +0100

    input output 7

---
+++ in_out_7.py
@@ -0,0 +1,7 @@
+# Write a program to take three names as input from the user in a single call
+# to the input() function.
+a=input("Enter three strings")
+c=0
+for i in a.split(" "):
+    c=c+1
+    print("Name",c,i)
