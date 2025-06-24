1b0b79f4db236430da6647fd4d914657db58b5cf
harika33333 <harika9653@gmail.com>
2025-06-24 19:08:19 +0100

    input output problem  11

--- in_out_11.py
+++ in_out_11.py
@@ -1,4 +1,7 @@
-num=int(input("Enter numerator: "))
-den=int(input("Enter denominator: "))
-per=(num/den)*100
-print("%.2f"% per,"%")
+num=int(input("Enter Numerator"))
+den=int(input("Enter denominator"))
+if(den==0):
+    print("denominator cannot be zero")
+else:
+    per=(num/den)*100
+    print("%.2f" % per+"%")
